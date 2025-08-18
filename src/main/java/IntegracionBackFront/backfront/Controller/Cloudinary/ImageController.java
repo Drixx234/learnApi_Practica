13911559e84ef44d17cb6cf9db7bea0b1ff7a2ab
Edgar-PTC <20230280@ricaldone.edu.sv>
2025-08-18 15:44:29 -0600

    Ya funciono

--- src/main/java/IntegracionBackFront/backfront/Controller/Cloudinary/ImageController.java
+++ src/main/java/IntegracionBackFront/backfront/Controller/Cloudinary/ImageController.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.Map;
 
 @RestController
@@ -29,6 +30,22 @@ public class ImageController {
                     "Url", imageURL
             ));
         }catch (Exception e){
+            return ResponseEntity.internalServerError().body("Error al subir la imagen" + e.getMessage());
+        }
+    }
+
+    @PostMapping("/upload-to-folder")
+    public ResponseEntity<?> uploadImagetoFolder(
+            @RequestParam("image") MultipartFile file,
+            @RequestParam String folder
+    ){
+        try {
+            String imageURL = cloudinaryService.uploadImage(file, folder);
+            return ResponseEntity.ok(Map.of(
+                    "message", "Imagen subida exitosamente",
+                    "Url", imageURL
+            ));
+        }catch (IOException e){
             return ResponseEntity.internalServerError().body("Error al subir la imagen" + e.getMessage());
         }
     }
